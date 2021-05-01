da58799eec880bdb88211e468817b9b6928503e6
engrvivs <22600345+engrvivs@users.noreply.github.com>
2021-05-01 17:10:36 +0530

    feat(version): set version in cmake and transfer through .h file

--- src/tutorial.cxx
+++ src/tutorial.cxx
@@ -1,9 +1,13 @@
 // A simple program that computes the squre root of a number
+#include "TutorialConfig.h"
 #include <cmath>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
+    // report version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
